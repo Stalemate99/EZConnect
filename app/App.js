1099b12c7e4e6335554e354b43fece8a116d70ca
Stalemate99 <sunilkanishq@gmail.com>
2021-05-09 20:54:45 +0530

    Initial commit

--- app/App.js
+++ app/App.js
@@ -4,7 +4,15 @@ export default function App() {
 
     return (
         <div>
-            Sample React Application
+            <button>Connect</button>
+            <button>Disconnect</button>
+            <p>Connected Devices</p>
+            <div>
+
+            </div>
+            <p>Connect to your device: </p>
+            <input type="text" />
+            <button>Find</button>
         </div>
     );
 }
